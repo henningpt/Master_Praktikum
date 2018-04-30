e8e01b8689f450bb3b243cd0edba42e9758c7ce5
henningpt <henning.ptaszyk@udo.edu>
2018-04-30 15:56:24 +0200

    + theorie , durchfuehrung , python program

--- v41/plot.py
+++ v41/plot.py
@@ -1,21 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+# lade daten
+radien_glas = np.genfromtxt("v41_glas.txt", unpack=True)
+radien_meta = np.genfromtxt("v41_metall.txt", unpack=True)
+radien_salz = np.genfromtxt("v41_salz.txt", unpack=True)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+# groessen definie.txren
+wavelen =
+camera_rad = 57.3 * 10**(-3)
+dist_probe = 130 * 10**(-3)
+
+
+# functions
+
+# x = np.linspace(0, 10, 1000)
+# y = x ** np.sin(x)
+#
+# plt.subplot(1, 2, 1)
+# plt.plot(x, y, label='Kurve')
+# plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+# plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+# plt.legend(loc='best')
+#
+# plt.subplot(1, 2, 2)
+# plt.plot(x, y, label='Kurve')
+# plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+# plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+# plt.legend(loc='best')
+#
+# # in matplotlibrc leider (noch) nicht möglich
+# plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+# plt.savefig('build/plot.pdf')
