2b14ea1f3b8a6fe472cbc7a5845a63a0b91651f8
henningpt <henning.ptaszyk@udo.edu>
2018-05-05 15:14:45 +0200

    + gemessene abstaende + winkel berechnen

--- v41/plot.py
+++ v41/plot.py
@@ -3,18 +3,47 @@ import numpy as np
 
 
 # lade daten
-radien_glas = np.genfromtxt("v41_glas.txt", unpack=True)
-radien_meta = np.genfromtxt("v41_metall.txt", unpack=True)
-radien_salz = np.genfromtxt("v41_salz.txt", unpack=True)
+r_meta = np.genfromtxt("metall.txt", unpack=True)
+r_salz = np.genfromtxt("salz.txt", unpack=True)
 
 
-# groessen definie.txren
-wavelen =
+# groessen definieren
+wavelen = 1.5417e-10
 camera_rad = 57.3 * 10**(-3)
-dist_probe = 130 * 10**(-3)
+d_probe = 130 * 10**(-3)
 
 
 # functions
+def winkel(d, R):
+    return (R / (2 * d))
+
+
+def test(m_sum, lamb, a):
+    m_sum = np.sqrt(m_sum)
+    return(180 * np.arcsin(m_sum * lamb/(2 * a)) / np.pi)
+
+
+print("Dies ist ein Test: ", test(18, wavelen, 3e-10))
+
+
+# rechnen
+# richtige radien
+r_meta = r_meta + 0.9
+r_salz = r_salz + 0.9
+
+# berechne zugehoerige winkel theta
+salz_winkel = winkel(d_probe, r_salz)
+meta_winkel = winkel(d_probe, r_meta)
+
+
+# auf konsole ausgeben
+print("\n\n\nwinkel salz:\n")
+for element in salz_winkel:
+    print(element)
+
+print("\n\n\nwinkel metall:\n")
+for element in meta_winkel:
+    print(element)
 
 # x = np.linspace(0, 10, 1000)
 # y = x ** np.sin(x)
