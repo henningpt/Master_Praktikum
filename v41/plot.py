e247dc7ba93f90d2085b9a4775691a29ee35ec10
henningpt <henning.ptaszyk@udo.edu>
2018-05-07 17:47:12 +0200

    was fuer ein metall ist probe 8?

--- v41/plot.py
+++ v41/plot.py
@@ -4,21 +4,21 @@ from scipy.optimize import curve_fit as cf
 import uncertainties.unumpy as unp
 from uncertainties.unumpy import (nominal_values as noms,
                                   std_devs as stds)
-
+# from tabelle.py import tabelle
 
 # lade daten
 r_meta = np.genfromtxt("metall.txt", unpack=True)
-r_salz = np.genfromtxt("salz.txt", unpack=True)
 
 
 bcc= np.array([[0, 1, 1],#2
+                [0, 0, 2],#4
                 [1, 1, 2],#6
                 [0, 2, 2],#8
                 [0, 1, 3],#10
+                [2, 2, 2],#12
                 [1, 2, 3],#14
                 [1, 1, 4],#18
-                [0, 2, 4],#20
-                [2, 3, 3]])#22
+                ])
 
 fcc=np.array([[1, 1, 1],#3
               [0, 0, 2],#4
@@ -30,6 +30,20 @@ fcc=np.array([[1, 1, 1],#3
               [0, 2, 4],#20
  ])
 
+
+dia = np.array([[1, 1, 1],#3
+              [0, 2, 2],#8
+              [1, 1, 3],#11
+              [0, 0, 4],#16
+              [1, 3, 3],#19
+              [2, 2, 4],#24
+              [3, 3, 3],#27
+              [4, 4, 0],#32
+ ])
+
+bcc=bcc.astype(np.float64)
+fcc=fcc.astype(np.float64)
+dia=dia.astype(np.float64)
 
 # groessen definieren
 wavelen = 1.5417e-10
@@ -62,15 +76,19 @@ def test(m_sum, lamb, a):
 # Gitterkonstante für jeden winkel berechnen
 def gitter(gitter, lamb, theta):
     m_sum = miller(gitter)
-    print(np.sin(theta))
+
+    print("\n\n\nm_sum aus miller aus gitter", m_sum)
     return(m_sum * lamb / (2 * np.sin(inrad(theta))))
 
 
 # Betragsquadrat der Millerindices
-def miller(bcc):
-    for ind, val in enumerate(bcc[:, 1]):
-        return(np.linalg.norm(bcc[ind, :])**2)
+def miller(struktur):
+    mill_arr = np.array(struktur[:,1])
 
+    for ind, val in enumerate(struktur[:, 1]):
+        mill_arr[ind] = np.linalg.norm(struktur[ind, :])
+        print("\nstruktur ", struktur[ind,:])
+    return(mill_arr)
 
 def gk_korrektur_a(gk, theta, proben_rad, camera_rad):
     return(gk * proben_rad * (1 - camera_rad) * np.cos(inrad(theta))**2 / (2 * camera_rad * inrad(theta)))
@@ -82,8 +100,9 @@ def winkel_korrektur(theta):
     lam_2 = 1.54478*10**(-10)
     return ((lam_1-lam_2)*np.tan(inrad(theta))/lam)
 
+
 def gk_korrektur_v(gk, theta, camera_rad, v):
-    return(gk * v * np.cos(inrad(theta))**2 / ( camera_rad))
+    return(gk * v * np.cos(inrad(theta))**2 / (camera_rad))
 # print("Dies ist ein Test: ", test(18, wavelen, 3e-10))
 
 
@@ -91,7 +110,7 @@ def gk_korrektur_v(gk, theta, camera_rad, v):
 def gk_plot(name, winkel, gk, funktion, korr, fitgrenzen):
     params, errors = cf(funktion, np.cos(inrad(winkel[fitgrenzen[0]:fitgrenzen[1]]))**2,
                                   noms(gk[fitgrenzen[0]:fitgrenzen[1]]))
-    g_plot = np.linspace(min(np.cos(inrad(winkel))**2), max(np.cos(inrad(winkel)**2)))
+    g_plot = np.linspace(0.1, max(np.cos(inrad(winkel)**2)))
     plt.errorbar(np.cos(inrad(winkel))**2, noms(gk), xerr=korr(winkel),
                  yerr=stds(gk), fmt='x')
     plt.plot(g_plot, funktion(g_plot, *params))
@@ -104,23 +123,21 @@ def gk_plot(name, winkel, gk, funktion, korr, fitgrenzen):
 # RECHNEN
 # richtige radien
 r_meta = r_meta + 0.9/2
-r_salz = r_salz + 0.9/2
 
 # berechne zugehoerige winkel theta
-salz_winkel = winkel(d_probe, r_salz)
 meta_winkel = winkel(d_probe, r_meta)
 
 # gitterkonstanten bcc metall
 gk_m_bcc = gitter(bcc, wavelen, meta_winkel)
 gk_m_fcc = gitter(fcc, wavelen, meta_winkel)
+gk_m_dia = gitter(dia, wavelen, meta_winkel)
+
 
 a_m_bcc=unp.uarray(gk_m_bcc,gk_korrektur_a(gk_m_bcc, meta_winkel, proben_rad, camera_rad) + gk_korrektur_v(gk_m_bcc, meta_winkel, camera_rad, v))
-a_m_fcc=unp.uarray(gk_m_fcc,gk_korrektur_a(gk_m_fcc, meta_winkel, proben_rad, camera_rad) + gk_korrektur_v(gk_m_bcc, meta_winkel, camera_rad, v))
+a_m_fcc=unp.uarray(gk_m_fcc,gk_korrektur_a(gk_m_fcc, meta_winkel, proben_rad, camera_rad) + gk_korrektur_v(gk_m_fcc, meta_winkel, camera_rad, v))
+a_m_dia=unp.uarray(gk_m_dia,gk_korrektur_a(gk_m_dia, meta_winkel, proben_rad, camera_rad) + gk_korrektur_v(gk_m_dia, meta_winkel, camera_rad, v))
 
 # auf konsole ausgeben
-print("\n\n\nwinkel salz:\n")
-for element in salz_winkel:
-    print(element)
 
 print("\n\n\nwinkel metall:\n")
 for element in meta_winkel:
@@ -132,11 +149,13 @@ for element in gk_m_bcc:
 
 
 print("\n\n\nGitterkonstanten fcc metall: ")
-
-
 for element in gk_m_fcc:
     print(element)
 
+
+print("\n\n\nGitterkonstanten dia metall: ")
+for element in gk_m_dia:
+    print(element)
 
 funp(" Korrekturen bcc: ", a_m_bcc)
 
@@ -147,10 +166,33 @@ def gerade(x,a,b):
 
 
 # fit plto fuer metall bcc
-a_bcc_end=gk_plot("bcc", meta_winkel, a_m_fcc, gerade, winkel_korrektur,[2,7])
-a_fcc_end=gk_plot("fcc", meta_winkel, a_m_bcc, gerade, winkel_korrektur,[2,7])
+a_bcc_end=gk_plot("bcc", meta_winkel, a_m_bcc, gerade, winkel_korrektur,[0,7])
+a_fcc_end=gk_plot("fcc", meta_winkel, a_m_fcc, gerade, winkel_korrektur,[0,7])
+a_dia_end=gk_plot("dia", meta_winkel, a_m_dia, gerade, winkel_korrektur,[1,7])
+
 print("\nMetall bcc",a_bcc_end)
 print(" \nMetall fcc",a_fcc_end)
+print(" \nMetall dia",a_dia_end)
+
+
+
+#TESTESTEST
+print("\n\n\n\nasldkfjölaksdflj", test(3, wavelen, 2.8665 * 10**(-10)))
+##################################################################################
+#salz
+
+# berechne zugehoerige winkel theta
+r_salz = np.genfromtxt("salz.txt", unpack=True)
+
+# richtige radien
+r_salz = r_salz + 0.9/2
+
+salz_winkel = winkel(d_probe, r_salz)
+
+print("\n\n\nwinkel salz:\n")
+for element in salz_winkel:
+    print(element)
+
 
 # x = np.linspace(0, 10, 1000)
 # y = x ** np.sin(x)
---
+++ v41/tabelle.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def tabelle(datensatz, Name,Rundungen):  # i=Spalten j=Zeilen
+    fobj_out = open(Name+".tex", "w")
+    i_max, j_max = np.shape(test)
+    fobj_out.write(r"\begin{table}"+"\n")
+    fobj_out.write(r"  \caption{}"+"\n")
+    fobj_out.write(r"  \label{}"+"\n")
+    fobj_out.write(r"\begin{tabular}{"+i_max*"S" +"} \n")
+    fobj_out.write(r"\toprule"+"\n \\\ \n")
+    fobj_out.write(r"\midrule"+"\n")
+    #runden und in strings umwandeln
+    for i in range(i_max):
+        datensatz[i][:] = np.around(datensatz[i][:], decimals=Rundungen[i])
+#    Werte=datensatz.astype(str)
+#    print(Werte)
+
+    for j in range(j_max):
+        for i in range(i_max):
+            formatierung='{0:.'+Rundungen[i].astype(str)+'f}'   # damit z.B. 0.30
+            fobj_out.write(formatierung.format(datensatz[i,j])) # nicht 0.3 ist
+            if(i<(i_max-1)):
+                fobj_out.write("\t&\t")
+        fobj_out.write(r"   \\ "+"\n")
+    # [j,i] = np.shape(test)
+    # for(i in )
+    fobj_out.write(r"\bottomrule"+"\n")
+    fobj_out.write(r"\end{tabular}"+"\n")
+    fobj_out.write(r"\end{table}"+"\n")
+    fobj_out.close()
+
+
+
+test=np.array([[2.2960,3.0100,4.400,5.500,6.600,7.700,8.800,9.900],[1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8]])
+test1=np.array([1,2,3,4,5])
+print(np.shape(test))
+print(test[0][7])
+name="test"
+print(np.around(test[0,:],decimals=3))
+tabelle(test,name,np.array([2,1]))
+Werte=test.astype(str)
+print("&".join(test[0,:].astype(str)))
+
+x=np.array(0.12345678)
+print("seltsam"+'{0:.5f}'.format(x)+'ja')
+print("round", np.around(x,5))
+
+x=np.array(0.1200001)
+print("seltsam"+'{0:.5f}'.format(x)+'ja')
+print("round", np.around(x,5))
+print("seltsam"+'{0:.5f}'.format(np.around(x,5))+'ja')
+x="6"
+mist='{0:.'+x+'f}'
+
+print(mist.format(test[0,0]))
