46ac59f503c989839683dae848d273f2690c9541
dagbjoern <dag.hering@tu-dortmund.de>
2018-07-10 00:57:27 +0200

    Auswertung 40%

--- v59/plot.py
+++ v59/plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from tabelle import tabelle
 # (a)
 
 
@@ -24,18 +24,18 @@ import numpy as np
 
 # (h)
 
-
+T=250e-9
 x = np.linspace(1e6,5e6,21)
 y =np.genfromtxt("messwerte_e.txt",unpack=True)
 # x = np.linspace(0, 10, 1000)
 # y = x ** np.sin(x)
+phase = (x*T*360)%360
 
+tabelle(np.array([x*1e-6,phase,y]),"phasenmessung",np.array([1,1,1]))
 
-
-plt.plot(x,y,'x' , label='Kurve')
+plt.plot(phase,y,'x' , label='Messwerte')
 plt.legend(loc='best')
-
-
 # in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+# plt.tight_layout()
 plt.savefig('build/plot.pdf')
