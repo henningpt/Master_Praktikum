80481f5ca2445a033066f1a3b863aef5145671ca
dagbjoern <dag.hering@tu-dortmund.de>
2018-07-09 17:08:54 +0200

    V59 cool pics

--- v59/plot.py
+++ v59/plot.py
@@ -1,20 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+x = np.linspace(1e6,5e6,21)
+y =np.genfromtxt("messwerte_e.txt",unpack=True)
+# x = np.linspace(0, 10, 1000)
+# y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+
+
+plt.plot(x,y,'x' , label='Kurve')
 plt.legend(loc='best')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
