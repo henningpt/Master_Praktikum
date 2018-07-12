07c8f8ff4f770b83b1be7074711ec9fc10de79b4
dagbjoern <dag.hering@tu-dortmund.de>
2018-07-12 14:25:56 +0200

    alles eine bisschen weiter

--- v59/plot.py
+++ v59/plot.py
@@ -1,19 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from tabelle import tabelle
-# (a)
+import uncertainties.unumpy as unp
+
+#a)
 
 
 # (b)
 
 
 # (c)
+#modulationsgerad Berechung
+print("Aufgabe c)\n\n")
+U_delta = unp.uarray(0.27,0.02)
+U_T = unp.uarray(2.6,0.1)/2
+U_M = unp.uarray(0.5,0.1)/2
+print("U_t=" , U_T)
+print("U_M=", U_M)
+m_c = U_delta / ( U_T * 2 )
 
+print("modulationsgerad bei aufgabenteil aus oszi c)", m_c)
+def dBm_in_Watt(x):
+    return 10**(x/10)*1e-3
+
+P_mitte = -9.528#in dBm
+P_links  = -30.38
+P_rechts = -30.37
+
+P_links  =dBm_in_Watt(P_links) #in dBm
+P_rechts = dBm_in_Watt(P_rechts)
+P_mitte = dBm_in_Watt(P_mitte)
+
+print("Spannung in sqrt(R)", np.sqrt(P_links),np.sqrt(P_mitte),np.sqrt(P_rechts) )
 
 # (d)
-
+#frequenzmodulierte schwingung
+# def momentan_f(t,f_t,f_m):
+#     return f_t (1 - )
+f_T=1e6
+f_M=1e5
+t_mess=250e-9
 
 # (e)
+
 
 
 # (f)
