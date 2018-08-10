9c4fd81544fd78e7f85e48d39ee51ceb5e091fc3
henningpt <henning.ptaszyk@udo.edu>
2018-08-10 16:51:13 +0200

    einige fortschritte erzielt.

--- v61/plot.py
+++ v61/plot.py
@@ -3,11 +3,8 @@ import numpy as np
 from tabelle import tabelle
 import uncertainties.unumpy as unp
 from scipy.optimize import curve_fit
-
+import scipy.constants as const
 # daten
-L = np.linspace(0,6)
-r_1 = 1.4
-r_2 = 1.4
 
 
 # gemessene intensitaeten
@@ -41,25 +38,46 @@ def wavelen(gconst, dist, dn):
 lambda80  = wavelen(0.001 / 80, lschirm, g80)
 lambda100 = wavelen(0.001 / 100, lschirm, g100)
 
-print("\nGITTER 80: ", lambda80)
-print("GITTER 80 MEAN: ", np.mean(lambda80))
-print("\nGITTER 100: ", lambda100)
-print("GITTER 100 MEAN: ", np.mean(lambda100))
+tabelle(np.array([unp.nominal_values(g80),unp.std_devs(g80),unp.nominal_values(lambda80*1e9), unp.std_devs(lambda80*1e9) ,unp.nominal_values(g100),unp.std_devs(g100),unp.nominal_values(lambda100*1e9),unp.std_devs(lambda100*1e9)]),"wellenlaenge_table",np.array([1,1,1,1,1,1,1,1]))
 
+
+print("\nGITTER 80: ", lambda80)
+print("GITTER 80 unp.mean: ", np.mean(lambda80))
+print("\nGITTER 100: ", lambda100)
+print("GITTER 100 unp.mean: ", np.mean(lambda100))
+
+
+#Stabilitätsbedingung
+L = np.linspace(0,4)
+r_1 = 1.4
+r_2 = 1.4
+plt.figure(1)
 plt.plot(L,(1-L/r_1)*(1-L/r_2) , '-' , label=r'g1g2_r_1=1.4')
 r_1=1
 plt.plot(L,(1-L/r_1)*(1-L/r_2) , '-' , label=r'g1g2_r_1=1')
 plt.plot(L,(1)*(1-L/r_2) , '-' , label=r'g1g2_r_1=999999')
-
-plt.hlines(1,0,2)
-plt.hlines(0,0,2)
-
+plt.hlines(1,0,4)
+plt.hlines(0,0,4)
 plt.legend(loc='best')
 # in matplotlibrc leider (noch) nicht möglich
 #plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 # plt.tight_layout()
 plt.savefig('build/plot_lange.pdf')
 plt.close()
+
+def g1g2(L):
+    r_1=1.4
+    r_2=1.4
+    return((1-L/r_1)*(1-L/r_2))
+
+laenge_laser, leistung_laser = np.genfromtxt("stabil.txt",unpack=True)
+laenge_laser*=1e-2
+print("lol",g1g2(laenge_laser)[g1g2(laenge_laser)<0])
+plt.figure(2)
+plt.plot(g1g2(laenge_laser), leistung_laser, 'x')
+plt.savefig("build/g1g2_Leistung.pdf")
+plt.close()
+tabelle(np.array([laenge_laser,g1g2(laenge_laser),leistung_laser]),"stabil_table",np.array([3,4,2]))
 
 #00 mode intensity
 
@@ -77,20 +95,27 @@ plt.legend(loc='best')
 # in matplotlibrc leider (noch) nicht möglich
 #plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.tight_layout()
-plt.savefig('build/intensity.pdf')
+plt.savefig('build/mode00.pdf')
 plt.close()
+print("\n \n Mode00fitparameter:",uparams_mode_00)
+
+tabelle(np.array([d_plot,I]),"mode00_table",np.array([1,1]))
+
 
 
 def polarisation(x,I_0,phi_0):
     return I_0 * np.cos(x * np.pi/180 - phi_0)**2
 x_pol = np.linspace(0,360)
-params_pol, cov_pol =curve_fit(polarisation,phi_plot,I_polar)
+params_pol, cov_pol =curve_fit(polarisation,phi_plot,I_polar,p0=[142,0])
 uparams_pol = unp.uarray(params_pol, np.sqrt(np.diag(cov_pol)))
 plt.plot(phi_plot, I_polar, 'x')
 plt.plot(x_pol,polarisation(x_pol,*params_pol),'-')
 plt.savefig('build/polarisation.pdf')
 plt.close()
 
+print("\n \n Polarisationsfitparameter:",uparams_pol)
+print("in degree:",uparams_pol[1]*180/np.pi)
+tabelle(np.array([phi_plot,I_polar]),"polarisation_table",np.array([0,1]))
 
 
 def mode01_asym(x,I_01,I_02,w1,w2,d_01,d_02):
@@ -108,7 +133,9 @@ plt.plot(d_plot_mode01, I_mode01, 'x')
 plt.plot(x_mode_01, mode01_asym(x_mode_01,*params_mode_01),'r')
 plt.savefig('build/mode01.pdf')
 plt.close()
+print("\n \n Mode01fitparameter:",uparams_mode_01)
 
+tabelle(np.array([d_plot_mode01,I_mode01]),"mode01_table",np.array([1,1]))
 
 #longitudiale moden
 
@@ -119,8 +146,21 @@ freq_L2=unp.uarray([188,375,559,746,934,1121,1305],np.ones(7)*5)*1e6
 delta_freq_L1= freq_L1[1:]-freq_L1[:-1]
 delta_freq_L2= freq_L2[1:]-freq_L2[:-1]
 
+L1 = 0.735
+L2 = 0.835
+
 print("\n\nFreq diff für L1",delta_freq_L1)
 print("\n\nFreq diff für L2",delta_freq_L2)
+# print("\n\nL1=",delta_freq_L1 * 2 / 3e8)
+# print("\n\nL2=",delta_freq_L2 * 2 / 3e8)
+print("vergleichs wert",const.c)
+longi_messung1 = 2 * delta_freq_L1 * L1
+longi_messung2 = 2 * delta_freq_L2 * L2
+print("longi_messung1:", longi_messung1)
+print("longi_messung2:", longi_messung2)
 
-print("\n\nL1=",delta_freq_L1 * 2 / 3e8)
-print("\n\nL2=",delta_freq_L2 * 2 / 3e8)
+print("\nmittelwert1: ",np.mean(longi_messung1))
+print("\nmittelwert2: ",np.mean(longi_messung2))
+
+print("\nrelative abweichung1 : ",(np.mean(longi_messung1)-const.c)/const.c)
+print("\nrelative abweichung2 : ",(np.mean(longi_messung2)-const.c)/const.c)
