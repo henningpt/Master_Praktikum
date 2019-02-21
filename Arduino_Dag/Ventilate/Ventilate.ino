ca2fd7eecaa24bfd40a118f6f7821ca324e54caa
dagbjoern <dag.hering@tu-dortmund.de>
2019-02-21 19:33:45 +0100

    arduino und Plots V65

--- Arduino_Dag/Ventilate/Ventilate.ino
+++ Arduino_Dag/Ventilate/Ventilate.ino
@@ -4,21 +4,64 @@ This sketch is for ventilate the room in an ordert way.
 const int flexPin = A0; //Define analog input pin to measure
             //flex sensor position. 
 
-int ventilate_time = 0;
+unsigned long ventilate_time_total = 5000;
+unsigned long ventilate_time_closed = 10000;   
+unsigned long time_open = 0;
+unsigned long time_closed = 0;
+unsigned long timer = 0;
+
+int flexPosition;  // Input value from the analog pin.
 
 void setup() {
   Serial.begin(9600); //Set serial baud rate to 9600 bps
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
   // put your setup code here, to run once:
 }
 
+
 void loop() {
-  int flexPosition;  // Input value from the analog pin.
  // put your main code here, to run repeatedly:
+ time_closed = time_difference(timer  ,millis()); 
+ Serial.print("Time: ");
+ Serial.println(millis()); //prints time since programm started
 
 // Read the position of the flex sensor (0 to 1023):
   flexPosition = analogRead(flexPin);
-  
+  if(ventilate_time_closed < time_closed ){ //Is it time to ventilate?
+  ventilate_process();
+  }     
+    
   Serial.print("sensor: ");
   Serial.println(flexPosition);
   delay(20);
 }
+
+unsigned long time_difference(unsigned long Start, unsigned long End){
+    unsigned long difference; 
+    difference = End - Start;
+    return difference; 
+  }
+
+void ventilate_process(){
+    while(flexPosition < 850){  // Is the window open?
+      digitalWrite(13, HIGH);   // Turn on the red LED till the window is opened
+    flexPosition = analogRead(flexPin); // read the position of the window
+    }
+   
+   digitalWrite(13, LOW);   // Turn off the red LED 
+   time_closed = 0;
+   timer = millis(); // set the time to the current time
+   
+   while(ventilate_time_total < time_open){  //Was the window long enough open?
+          time_open = time_difference(timer, millis());   
+   }
+   while(flexPosition > 850){  // Is the window closed?
+          digitalWrite(12, HIGH);   // Turn on the yellow LED till the window is closed
+          flexPosition = analogRead(flexPin); // read the position of the window
+   }           
+   
+   digitalWrite(12, LOW);    // Turn off the yellow LED   
+   timer = millis();    // set the new timer
+  }  
+  
--- v65/plot.py
+++ v65/plot.py
@@ -1,21 +1,105 @@
 import matplotlib.pyplot as plt
 import numpy as np
+# from tabelle import tabelle
+import uncertainties.unumpy as unp
+from scipy.optimize import curve_fit
+import scipy.constants as const
+# daten
+# gemessene intensitaeten
+THETA_det_scan, Int_det_scan   = np.genfromtxt("messwerte/det_scan.txt", unpack=True)
+theta_untergrund , int_untergrund = np.genfromtxt("messwerte/mess1_untergrund.txt", unpack=True)
+theta_messung , int_messung = np.genfromtxt("messwerte/mess1.txt", unpack=True)
+Theta_rock , Int_rock = np.genfromtxt("messwerte/rock_scan_0.txt", unpack=True)
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+#Winkel < 0.25 werden vernachlässigt
+# da der Strahl noch teilweise 
+# direkt auf den Detektor fällt.
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+Theta_messung = theta_messung[theta_messung > 0.25]
+Int_messung = int_messung[theta_messung > 0.25]
+Theta_untergrund  = theta_untergrund[theta_untergrund > 0.25]
+Int_untergrund = int_untergrund[theta_untergrund > 0.25]
+
+#Gausfunktion
+
+def Gaus(x,mu,sigma):
+    return 1/np.sqrt(2 * np.pi *sigma**2) * np.exp(-(x-mu)**2 /(2 * sigma**2) )
+
+# Brechungsindex
+n1 = 1 #Luft
+n2 = 1 - 1e-6
+n3 = 1 - 2e-6
+
+#Rauigkeit
+
+sigma1 = 8e-10 #Schicht
+sigma2 = 3e-10 #Substrat 
+
+
+#Schichtdicke
+
+z2 = 500e-10
+
+#Einfallswinkel
+ai = np.array(Theta_messung * np.pi / 180 )
+
+# Wellenvektorübertrag
+
+qz = 4 * np.pi / 1.54 * np.sin(ai)
+
+#Betrag des Wellenvektors
+
+k = 2 * np.pi / 1.54 * 1e10
+# z-Komponenten 
+
+kz1 = k * np.sqrt(n1**2 - np.cos(ai)**2)
+kz2 = k * np.sqrt(n2**2 - np.cos(ai)**2)
+kz3 = k * np.sqrt(n3**2 - np.cos(ai)**2)
+
+
+params_det, cov_det = curve_fit(Gaus ,THETA_det_scan,Int_det_scan,)
+uparams_pol = unp.uarray(params_pol, np.sqrt(np.diag(cov_pol)))
+
+
+plt.figure(1)
+plt.plot(THETA_det_scan, Int_det_scan, label='Kurve')
+plt.xlabel(r'$\Theta$')
+plt.ylabel(r'$Intesity$')
 plt.legend(loc='best')
+plt.savefig('build/plot_det_scan.pdf')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+
+
+plt.figure(2)
+plt.plot(Theta_untergrund, Int_untergrund, label='Kurve')
+plt.xlabel(r'$\Theta$')
+plt.ylabel(r'$Intesity$')
 plt.legend(loc='best')
+plt.savefig('build/plot_untergrund.pdf')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.figure(3)
+plt.plot(Theta_messung, Int_messung,'x' ,label='Kurve')
+plt.xlabel(r'$\Theta$')
+plt.ylabel(r'$Intesity$')
+plt.legend(loc='best')
+plt.savefig('build/plot_messung.pdf')
+
+plt.figure(4)
+plt.plot(Theta_rock, Int_rock, label='Kurve')
+plt.xlabel(r'$\Theta$')
+plt.ylabel(r'$Intesity$')
+plt.legend(loc='best')
+plt.savefig('build/plot_rocking.pdf')
+
+plt.figure(5)
+plt.plot(Theta_messung, Int_messung-Int_untergrund,'-', label='Differenz')
+plt.plot(Theta_messung, Int_messung,'--' ,label='Reflektivitätsscan')
+plt.plot(Theta_untergrund, Int_untergrund,'-', label='Diffuser Scan')
+plt.xlabel(r'$\Theta$')
+plt.ylabel(r'$Intesity$')
+plt.yscale('log')
+plt.legend(loc='best')
+plt.savefig('build/plot_messung_untergrund.pdf')
+
+
+
