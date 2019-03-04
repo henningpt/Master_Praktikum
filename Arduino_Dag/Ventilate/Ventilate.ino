960dfd0363509876fd672d0b22f75495c72da2b8
dagbjoern <dag.hering@tu-dortmund.de>
2019-03-04 19:25:22 +0100

    rest der theorie

--- Arduino_Dag/Ventilate/Ventilate.ino
+++ Arduino_Dag/Ventilate/Ventilate.ino
@@ -4,36 +4,60 @@ This sketch is for ventilate the room in an ordert way.
 const int flexPin = A0; //Define analog input pin to measure
             //flex sensor position. 
 
-unsigned long ventilate_time_total = 5000;
+unsigned long ventilate_time_total = 1000;
 unsigned long ventilate_time_closed = 10000;   
 unsigned long time_open = 0;
 unsigned long time_closed = 0;
 unsigned long timer = 0;
-
+const int RED_PIN = 9;
+const int GREEN_PIN = 10;
+const int BLUE_PIN = 11;
 int flexPosition;  // Input value from the analog pin.
+const int button_pin = 2;  // pushbutton 2 pin
+int button_state;
+int season = 0; 
 
 void setup() {
   Serial.begin(9600); //Set serial baud rate to 9600 bps
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT); 
+  pinMode(12, OUTPUT);  
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(button_pin, INPUT);
+
   // put your setup code here, to run once:
 }
 
 
 void loop() {
  // put your main code here, to run repeatedly:
+
+button_state = digitalRead(button_pin);
+
+// if button is pressed
+ if(button_state == LOW){
+  season +=1;
+  season = season%4; 
+  season_set(season);
+ }
  time_closed = time_difference(timer  ,millis()); 
- Serial.print("Time: ");
- Serial.println(millis()); //prints time since programm started
+// Serial.print("Time: ");
+// Serial.println(millis()); //prints time since programm started
 
 // Read the position of the flex sensor (0 to 1023):
   flexPosition = analogRead(flexPin);
+  Serial.print("Zeit geschlossen: ");
+  Serial.print(ventilate_time_closed);
+  Serial.print("/");
+  Serial.println(time_closed);
   if(ventilate_time_closed < time_closed ){ //Is it time to ventilate?
   ventilate_process();
   }     
     
-  Serial.print("sensor: ");
-  Serial.println(flexPosition);
+//  Serial.print("sensor: ");
+//  Serial.println(flexPosition);
+  
   delay(20);
 }
 
@@ -44,19 +68,25 @@ unsigned long time_difference(unsigned long Start, unsigned long End){
   }
 
 void ventilate_process(){
-    while(flexPosition < 850){  // Is the window open?
+    while(flexPosition < 800){  // Is the window open?
       digitalWrite(13, HIGH);   // Turn on the red LED till the window is opened
-    flexPosition = analogRead(flexPin); // read the position of the window
+      flexPosition = analogRead(flexPin); // read the position of the window
     }
    
    digitalWrite(13, LOW);   // Turn off the red LED 
    time_closed = 0;
    timer = millis(); // set the time to the current time
-   
-   while(ventilate_time_total < time_open){  //Was the window long enough open?
-          time_open = time_difference(timer, millis());   
+  
+   while(ventilate_time_total > time_open){  //Was the window long enough open?
+      Serial.print("Zeit geöffnet: ");
+      Serial.print(ventilate_time_total);
+      Serial.print("/");
+      Serial.println(time_open);
+
+      time_open = time_difference(timer, millis());   
    }
-   while(flexPosition > 850){  // Is the window closed?
+   time_open = 0;
+   while(flexPosition > 800){  // Is the window closed?
           digitalWrite(12, HIGH);   // Turn on the yellow LED till the window is closed
           flexPosition = analogRead(flexPin); // read the position of the window
    }           
@@ -64,4 +94,46 @@ void ventilate_process(){
    digitalWrite(12, LOW);    // Turn off the yellow LED   
    timer = millis();    // set the new timer
   }  
-  
+
+
+void season_set(int x){ // where x is the account how often he butten was pressed 
+     int display_time = 2000;
+  switch (x) {
+    case 0: // spring
+      // green
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(BLUE_PIN, LOW);
+    ventilate_time_total = 5000;
+    ventilate_time_closed = 10000;   
+    break;
+  case 1: // summer
+    // organe
+    analogWrite(RED_PIN, 255);
+    analogWrite(GREEN_PIN, 165);
+    analogWrite(BLUE_PIN, 0 );
+    ventilate_time_total = 100;
+    ventilate_time_closed = 100000;   
+    break;
+  case 2: // autumn
+    // red
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(BLUE_PIN, LOW);
+    ventilate_time_total = 700;
+    ventilate_time_closed = 100000;   
+    break;
+  case 3: // winter
+    // blue
+    analogWrite(RED_PIN, 5);
+    analogWrite(GREEN_PIN, 215);
+    analogWrite(BLUE_PIN, 255);
+    ventilate_time_total = 10;
+    ventilate_time_closed = 50000;   
+    break;
+  }
+  delay(display_time);
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(BLUE_PIN, LOW);
+}
